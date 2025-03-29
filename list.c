1abb518e5589cdace417394118c9aaeebee00176
Your Name <you@example.com>
2025-03-28 22:32:49 -0300

    new try

--- list.c
+++ list.c
@@ -62,6 +62,13 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, void * data) {
+    Node* nuevoNodo = (Node*) malloc(sizeof(Node));
+    nuevoNodo->data = data;
+    nuevoNodo->prev = NULL;
+    nuevoNodo->next = list->head;
+    if (list->head) list->head->prev = nuevoNodo;
+    list->head = nuevoNodo;
+    list->size ++;
 }
 
 void pushBack(List * list, void * data) {
